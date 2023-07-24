dcc431b89f0c9df208bf9cb7aa62d0e778736ef2
Jared Stehler <jstehler@hubspot.com>
2023-07-24 13:54:18 -0400

    derp

--- prettier-maven-plugin/src/main/java/com/hubspot/maven/plugins/prettier/PrettierArgs.java
+++ prettier-maven-plugin/src/main/java/com/hubspot/maven/plugins/prettier/PrettierArgs.java
@@ -1,11 +1,5 @@
 package com.hubspot.maven.plugins.prettier;
 
-import com.google.common.io.Resources;
-import com.hubspot.maven.plugins.prettier.internal.NodeDownloader;
-import com.hubspot.maven.plugins.prettier.internal.NodeInstall;
-import com.hubspot.maven.plugins.prettier.internal.OperatingSystemFamily;
-import com.hubspot.maven.plugins.prettier.internal.PrettierDownloader;
-import com.hubspot.maven.plugins.prettier.internal.PrettierPatcher;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
@@ -16,7 +10,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+
 import javax.annotation.Nullable;
+
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
@@ -24,6 +20,13 @@ import org.apache.maven.plugin.descriptor.PluginDescriptor;
 import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
+
+import com.google.common.io.Resources;
+import com.hubspot.maven.plugins.prettier.internal.NodeDownloader;
+import com.hubspot.maven.plugins.prettier.internal.NodeInstall;
+import com.hubspot.maven.plugins.prettier.internal.OperatingSystemFamily;
+import com.hubspot.maven.plugins.prettier.internal.PrettierDownloader;
+import com.hubspot.maven.plugins.prettier.internal.PrettierPatcher;
 
 public abstract class PrettierArgs extends AbstractMojo {
   /**
@@ -139,7 +142,7 @@ public abstract class PrettierArgs extends AbstractMojo {
       if (disableGenericsLinebreaks) {
         if (prettierJavaVersion.startsWith("2")) {
           URL patch = Resources.getResource("no-linebreak-generics.patch");
-          return new PrettierPatcher(prettierJava, getLog()).patch(patch, prettierJavaVersion);
+          return new PrettierPatcher(prettierJava, getLog(), patchCommand).patch(patch, prettierJavaVersion);
         } else if ("1.5.0".compareTo(prettierJavaVersion) > 0) {
           // versions before 1.5.0 don't linebreak generics
           return prettierJava;
