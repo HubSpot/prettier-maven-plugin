9beb0ba8a863386f81d9f536e9da66e1f0223ef4
Jared Stehler <jstehler@hubspot.com>
2023-07-24 13:55:31 -0400

    revert import sorting

--- prettier-maven-plugin/src/main/java/com/hubspot/maven/plugins/prettier/PrettierArgs.java
+++ prettier-maven-plugin/src/main/java/com/hubspot/maven/plugins/prettier/PrettierArgs.java
@@ -1,5 +1,11 @@
 package com.hubspot.maven.plugins.prettier;
 
+import com.google.common.io.Resources;
+import com.hubspot.maven.plugins.prettier.internal.NodeDownloader;
+import com.hubspot.maven.plugins.prettier.internal.NodeInstall;
+import com.hubspot.maven.plugins.prettier.internal.OperatingSystemFamily;
+import com.hubspot.maven.plugins.prettier.internal.PrettierDownloader;
+import com.hubspot.maven.plugins.prettier.internal.PrettierPatcher;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
@@ -10,9 +16,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
-
 import javax.annotation.Nullable;
-
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
@@ -20,13 +24,6 @@ import org.apache.maven.plugin.descriptor.PluginDescriptor;
 import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
-
-import com.google.common.io.Resources;
-import com.hubspot.maven.plugins.prettier.internal.NodeDownloader;
-import com.hubspot.maven.plugins.prettier.internal.NodeInstall;
-import com.hubspot.maven.plugins.prettier.internal.OperatingSystemFamily;
-import com.hubspot.maven.plugins.prettier.internal.PrettierDownloader;
-import com.hubspot.maven.plugins.prettier.internal.PrettierPatcher;
 
 public abstract class PrettierArgs extends AbstractMojo {
   /**
